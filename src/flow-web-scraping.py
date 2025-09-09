06a25d2637417cc31dc86b06eed58adeb19a3e7b
Juliana <julianasalustianovieira@gmail.com>
2025-09-09 11:52:22 -0300

    Fix: Corrigido erro de lógica na função scrape(links), onde o laço for não estava definido, e somente um imóvel estava sendo salvo.

--- src/flow-web-scraping.py
+++ src/flow-web-scraping.py
@@ -51,90 +51,84 @@ def extrair_links_anuncios(base_url):
     return links
 
 @task(retries=2, retry_delay_seconds=3)
-def extrair_info_imovel(links):
+def extrair_info_imovel(link):
 
     imovel = {}
 
-    for link in links:
-        soup = obter_html(link)
+    soup = obter_html(link)
 
-        # Código do anúncio
-        imovel['cod_anuncio'] = link.split("-")[-1]
+    # Código do anúncio
+    imovel['cod_anuncio'] = link.split("-")[-1]
 
-        # Título e Descrição
-        div_desc = soup.find('div', id='description-title').find_all({'span': 'data-ds-component="DS-Text'})
+    # Título e Descrição
+    div_desc = soup.find('div', id='description-title').find_all({'span': 'data-ds-component="DS-Text'})
 
-        if div_desc:
-            imovel['titulo'] = div_desc[0].text
-            imovel['descricao'] = div_desc[1].text
-        
-        else:
-            imovel['titulo'] = "N/A"
-            imovel['descricao'] = "N/A"
+    if div_desc:
+        imovel['titulo'] = div_desc[0].text
+        imovel['descricao'] = div_desc[1].text    
 
-        # Valores de Aluguel, IPTU e Condomínio
-        span_precos = soup.find('div', id='price-box-container').find_all('span')
+    else:
+        imovel['titulo'] = "N/A"
+        imovel['descricao'] = "N/A"
 
-        if span_precos:
+    # Valores de Aluguel, IPTU e Condomínio
+    span_precos = soup.find('div', id='price-box-container').find_all('span')
 
-            itens = [valor.text for valor in span_precos] 
+    if span_precos:
+        itens = [valor.text for valor in span_precos] 
+        precos = {}
+        chave = None
+        valor = None
 
-            precos = {}
-            chave = None
-            valor = None
+        for item in itens:
+            if item in ['Aluguel', 'Condomínio', 'IPTU']:
+                chave = item
+            elif item.startswith("R$"):
+                valor = item
+            if chave == None:
+                continue
 
-            for item in itens:
-                if item in ['Aluguel', 'Condomínio', 'IPTU']:
-                    chave = item
-                elif item.startswith("R$"):
-                    valor = item
+            precos[chave] = valor
 
-                if chave == None:
-                    continue
+        imovel['precos'] = precos 
 
-                precos[chave] = valor
+    else:
+        imovel['precos'] = "N/A"
 
-            imovel['precos'] = precos
-        
-        else:
-            imovel['precos'] = "N/A"
+    # Endereço do imóvel
+    span_localizacao = soup.find('div', id='location').find_all('span')[1:-2]
 
-        # Endereço do imóvel
-        span_localizacao = soup.find('div', id='location').find_all('span')[1:-2]
+    if span_localizacao:
+        imovel['endereco'] = [end.text for end in span_localizacao]
 
-        if span_localizacao:
-            imovel['endereco'] = [end.text for end in span_localizacao]
-        else:
-            imovel['endereco'] = "N/A"
+    else:
+        imovel['endereco'] = "N/A"
 
-        
-        # Características do imóvel e do condomínio
-        span_pagina = soup.find_all({'span': 'data-ds-component="DS-Text'})
+    # Características do imóvel e do condomínio
+    span_pagina = soup.find_all({'span': 'data-ds-component="DS-Text'})
 
-        texto_pagina = [texto.text for texto in span_pagina if texto.text not in 'Fechar janela de diálogo']
-        texto_pagina = "|".join(texto_pagina)
+    texto_pagina = [texto.text for texto in span_pagina if texto.text not in 'Fechar janela de diálogo']
 
-        if texto_pagina:
+    texto_pagina = "|".join(texto_pagina)
 
-            try: 
-                imovel['carac_imovel'] = list(set(texto_pagina.split("Características do imóvel")[1].split("|")))
-                imovel['carac_cond'] = list(set(texto_pagina.split("Características do condomínio")[1].split("|")))
+    if texto_pagina:
+        try: 
+            imovel['carac_imovel'] = list(set(texto_pagina.split("Características do imóvel")[1].split("|")))
+            imovel['carac_cond'] = list(set(texto_pagina.split("Características do condomínio")[1].split("|")))
 
-            except:
-                imovel['carac_imovel'] = "N/A"
-                imovel['carac_cond'] = "N/A"
+        except:
+            imovel['carac_imovel'] = "N/A"
+            imovel['carac_cond'] = "N/A"
 
-        # Data do anúncio
-        regex_data = r'\b\d{2}/\d{2}\s*às\s*\d{2}:\d{2}\b'
-        data = re.findall(regex_data, texto_pagina)
-        
-        if data:
-            imovel['data_anuncio'] = str(data[0])
-            
-        else:
-            imovel['data_anuncio'] = "N/A"
+    # Data do anúncio
+    regex_data = r'\b\d{2}/\d{2}\s*às\s*\d{2}:\d{2}\b'
 
-        aguardar()
+    data = re.findall(regex_data, texto_pagina)        
+    if data:
+        imovel['data_anuncio'] = str(data[0])
+
+    else:
+        imovel['data_anuncio'] = "N/A"
 
     return imovel
 
@@ -143,10 +137,10 @@ def scrape(links):
 
     dados_completos = []
     
-    resultado = extrair_info_imovel(links)
-    dados_completos.append(resultado)
-
-    aguardar()
+    for link in links:
+        resultado = extrair_info_imovel(link)
+        dados_completos.append(resultado)
+        aguardar()
 
     return dados_completos
 
